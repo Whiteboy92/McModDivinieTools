0123bc228ab21757ad425e9808da8105be04983f
Whiteboy/Marcin <divinemonster123@gmail.com>
2022-09-12 11:03:13 +0200

    Update CustomAxeItem.java, CustomHoeItem.java, and 3 more files...

--- src/main/java/net/whiteboy/divine_tools/item/ModItems.java
+++ src/main/java/net/whiteboy/divine_tools/item/ModItems.java
@@ -1,15 +1,34 @@
 package net.whiteboy.divine_tools.item;
 
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
-import net.minecraft.item.Item;
+import net.minecraft.item.*;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 import net.whiteboy.divine_tools.DivineToolsMod;
+import net.whiteboy.divine_tools.custom.CustomAxeItem;
+import net.whiteboy.divine_tools.custom.CustomHoeItem;
+import net.whiteboy.divine_tools.custom.CustomPickaxeItem;
+import net.whiteboy.divine_tools.materials.DivinityIngotToolsMaterial;
 
 public class ModItems {
 
     public static final Item RAW_DIVINITY_ORE = registerItem("raw_divinity_ore",
             new Item(new FabricItemSettings().group(ModItemGroup.DIVINE_TOOLS)));
+
+    public static final Item DIVINITY_SHOVEL = registerItem("divinity_shovel", new ShovelItem(DivinityIngotToolsMaterial.INSTANCE,
+            1.5F, -3.0F, new Item.Settings().group(ModItemGroup.DIVINE_TOOLS)));
+
+    public static final Item DIVINITY_SWORD = registerItem("divinity_sword", new SwordItem(DivinityIngotToolsMaterial.INSTANCE,
+            10, -2.0F, new Item.Settings().group(ModItemGroup.DIVINE_TOOLS)));
+
+    public static final Item DIVINITY_PICKAXE = registerItem("divinity_pickaxe", new CustomPickaxeItem(DivinityIngotToolsMaterial.INSTANCE,
+            1, -2.4F, new Item.Settings().group(ModItemGroup.DIVINE_TOOLS)));
+
+    public static final Item DIVINITY_AXE = registerItem("divinity_axe", new CustomAxeItem(DivinityIngotToolsMaterial.INSTANCE,
+            12, -2.4F, new Item.Settings().group(ModItemGroup.DIVINE_TOOLS)));
+
+    public static final Item DIVINITY_HOE = registerItem("divinity_hoe", new CustomHoeItem(DivinityIngotToolsMaterial.INSTANCE,
+            7, -3.2F, new Item.Settings().group(ModItemGroup.DIVINE_TOOLS)));
 
 
     public static void registerModItems(){
---
+++ src/main/java/net/whiteboy/divine_tools/materials/DivinityIngotToolsMaterial.java
@@ -0,0 +1,42 @@
+package net.whiteboy.divine_tools.materials;
+
+import net.minecraft.item.Item;
+import net.minecraft.item.ToolMaterial;
+import net.minecraft.recipe.Ingredient;
+import net.whiteboy.divine_tools.item.DivinityIngotItem;
+import net.whiteboy.divine_tools.item.RegisterArmorItems;
+
+public class DivinityIngotToolsMaterial implements ToolMaterial {
+
+    public static final DivinityIngotToolsMaterial INSTANCE = new DivinityIngotToolsMaterial();
+
+    @Override
+    public int getDurability() {
+        return 3333;
+    }
+
+    @Override
+    public float getMiningSpeedMultiplier() {
+        return 12.0f;
+    }
+
+    @Override
+    public float getAttackDamage() {
+        return 0;
+    }
+
+    @Override
+    public int getMiningLevel() {
+        return 5;
+    }
+
+    @Override
+    public int getEnchantability() {
+        return 100;
+    }
+
+    @Override
+    public Ingredient getRepairIngredient() {
+        return Ingredient.ofItems(RegisterArmorItems.DIVINITY_INGOT);
+    }
+}
